cd54613469cda53952533dc22654f122f6036fc3
Harrison Van Roy <harrison.vanroy007@gmail.com>
2021-09-10 22:37:31 +1000

    add lint, cleanup ts

--- server/src/util/Trie.ts
+++ server/src/util/Trie.ts
@@ -1,36 +1,45 @@
-// import TrieSearch from 'trie-search';
-import { create, lookup } from '@remusao/trie';
-import fs from 'fs';
+import { create, lookup, Trie } from "@remusao/trie";
+import fs from "fs";
 
 type Props = {
   words: Set<string>;
   characters: string;
-}
+};
 
 const props: Props = {
   words: new Set(),
-  characters: "NFIOISSOD".toLowerCase()
+  characters: "NFIOISSOD".toLowerCase(),
 };
 
-let data = fs.readFileSync('./src/util/dict.txt', 'utf8').split('\n');
+const data = fs.readFileSync("./src/util/dict.txt", "utf8").split("\n");
 
-const trie = create(data)
+const trie = create(data);
 
 const populateSet = (props: Props) => {
   // const trie = new TrieSearch('name', { min: 1 });
 
-  const permuteWords = (wordBank, currWord, wordList, trie) => {
+  const permuteWords = (
+    wordBank: string,
+    currWord: string,
+    wordList: Set<string>,
+    trie: Trie
+  ) => {
     if (lookup(trie, currWord)) {
       wordList.add(currWord);
     }
 
     for (const letter of wordBank) {
-      permuteWords(wordBank.replace(letter, ''), currWord + letter, wordList, trie);
+      permuteWords(
+        wordBank.replace(letter, ""),
+        currWord + letter,
+        wordList,
+        trie
+      );
     }
-  }
+  };
 
   permuteWords(props.characters, "", props.words, trie);
-}
+};
 
 populateSet(props);
 const wordList = Array.from(props.words);
