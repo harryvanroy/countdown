bb795f38a2ef29bc7277a90b0ebe1374d0b369d2
TripleExclam <burtonmatthew00@gmail.com>
2021-09-11 00:16:55 +1000

    cleaned up code

--- server/src/util/numbers.ts
+++ server/src/util/numbers.ts
@@ -22,24 +22,11 @@ var OPS: Record<string, (n1: number, n2: number) => number> = {
 };
 
 const permute_numbers = (numbers: number[], valSums: number, equation: string, equations: Record<string, string[]>) => {
-    if (numbers.length == 0) {
-        return
-    } else if (numbers.length == 1) {
-        for (var o in OPS) {
-            var s = OPS[o](valSums, numbers[0]);
-            var term = "(" + equation + ")" + o + numbers[0];
-            if (s > 0 && s < 1000 && s >> 0 == s) {
-                equations[s].push(term);
-            }
-        }
-        return
-    }
-
     for (var m in OPS) {
         for (var i = 0; i < numbers.length; i++) {
             var ni = numbers[i];
             var r = OPS[m](valSums, ni);
-            var equi = "(" + equation + ")" + m + ni;
+            var equi = "(" + equation + ") " + m + " " + ni;
             if (equation == "") {
                 r = ni;
                 equi = "" + ni;
@@ -59,4 +46,4 @@ for (let i = 0; i < 1000; i++) {
 
 permute_numbers(numbers, 0, "", yeet);
 
-console.log(yeet["334"]);
+console.log(yeet["333"]);
