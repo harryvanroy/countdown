e340e525f3c21767df12e997b8bc03ea1c9920de
TripleExclam <burtonmatthew00@gmail.com>
2021-09-10 20:07:12 +1000

    words done

--- server/src/util/Trie.ts
+++ server/src/util/Trie.ts
@@ -1,11 +1,40 @@
-import TrieSearch from 'trie-search';
+// import TrieSearch from 'trie-search';
+import { create, lookup } from '@remusao/trie';
+import fs from 'fs';
 
-const trie = new TrieSearch();
+type Props = {
+  words: Set<string>;
+  characters: string;
+}
 
-trie.map('hello', 'world'); // Map 'hello' to the String 'world'
-trie.map('here', 'is a trie search'); // Map 'hello' to the String 'world'
+const props: Props = {
+  words: new Set(),
+  characters: "NFIOISSOD".toLowerCase()
+};
 
-trie.search('he');    // ['world', 'is a trie search]
-trie.search('her');   // ['is a trie search]
-trie.search('hel');   // ['world']
-console.log(trie.search('hello')); // ['world']
+let data = fs.readFileSync('./src/util/dict.txt', 'utf8').split('\n');
+
+const trie = create(data)
+
+const populateSet = (props: Props) => {
+  // const trie = new TrieSearch('name', { min: 1 });
+
+  const permuteWords = (wordBank, currWord, wordList, trie) => {
+    if (lookup(trie, currWord)) {
+      wordList.add(currWord);
+    }
+
+    for (const letter of wordBank) {
+      permuteWords(wordBank.replace(letter, ''), currWord + letter, wordList, trie);
+    }
+  }
+
+  permuteWords(props.characters, "", props.words, trie);
+}
+
+populateSet(props);
+const wordList = Array.from(props.words);
+wordList.sort(function (a, b) {
+  return b.length - a.length;
+});
+console.log(wordList);
