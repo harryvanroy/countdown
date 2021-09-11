e930931bc7cbfbec804a047a2cf2a9a9fbbf9d0e
Harrison Van Roy <harrison.vanroy007@gmail.com>
2021-09-11 19:06:53 +1000

    WIP

--- client/src/components/Numbers.tsx
+++ client/src/components/Numbers.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, TextField, Paper } from "@material-ui/core";
+import { Box, TextField, Paper, Button } from "@material-ui/core";
 import { useGame } from "../context/game";
 
 const useStyles = makeStyles({
@@ -14,30 +14,39 @@ const useStyles = makeStyles({
 });
 
 export const Numbers = () => {
+  const [answer, setAnswer] = useState("");
+  const [currentTotal, setTotal] = useState(0);
   const classes = useStyles();
   const game = useGame();
+
+  const handleCheckAnswer = () => {
+    const answerSafe = answer.replace(/[^-()\d/*+.]/g, "");
+    console.log(answerSafe);
+    setTotal(eval(answerSafe));
+  };
+
+  const handleAnswerChange = (event: any) => {
+    setAnswer(event.target.value);
+    console.log(event.target.value);
+  };
 
   return (
     <Box className={classes.root}>
       <Paper>
         <TextField
-          className={classes.workingOut}
-          id="outlined-multiline-static"
-          multiline
-          rows={16}
-          defaultValue=""
-          variant="outlined"
-        />
-        <TextField
           id="outlined-multiline-static"
           multiline
           rows={3}
-          defaultValue=""
+          value={answer}
           variant="outlined"
+          onChange={handleAnswerChange}
         />
-        <input type="submit" value="Submit" />
+        <Button variant="contained" onClick={handleCheckAnswer}>
+          Check answer
+        </Button>
         <p>Selection: {game?.state.selection?.join()}</p>
         <p>Target: {game?.state.targetNum}</p>
+        <p>Current value: {currentTotal}</p>
       </Paper>
     </Box>
   );
